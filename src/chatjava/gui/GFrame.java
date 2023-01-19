71042f7de0c03d1ff9ed75905a6cb45d061c2779
Michele Pagano <mpagano284@gmail.com>
2023-01-19 09:33:48 +0100

    Added chatArea and scrollBars

--- src/chatjava/gui/GFrame.java
+++ src/chatjava/gui/GFrame.java
@@ -1,9 +1,16 @@
 package chatjava.gui;
 
 import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
 public class GFrame extends JFrame {
     public GFrame() {
-
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setSize(800, 600);
+        this.setResizable(false);
+        this.setContentPane(new GPanel());
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        this.setTitle("ChatGPT pezzotto");
     }
 }
