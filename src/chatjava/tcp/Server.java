ad2d1f06843036db51911f31b25299933987bfb0
Michele Pagano <mpagano284@gmail.com>
2023-02-19 16:24:46 +0100

    Added Server.connect() code

--- src/chatjava/tcp/Server.java
+++ src/chatjava/tcp/Server.java
@@ -1,20 +1,76 @@
 package chatjava.tcp;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server implements ConnectionInterface {
     private static Server instance = null;
+    private ServerSocket serv;
 
     private Server() {
+    }
 
+    public static Server getInstance() {
+        if(instance == null) {
+            instance = new Server();
+        }
+        return instance;
+    }
+
+    private boolean createServer(final int port) {
+        try {
+            serv = new ServerSocket(port);
+        } catch (IOException ie) {
+            return false;
+        }
+
+        return true;
     }
 
     @Override
-    public void connect(String ipAddress, int port) {
+    public void connect(final String ipAddress, final int port) {
+        if(!this.createServer(port)) {
+            System.err.println("Qualcosa e' andato storto (Creazione server)");
+        }
 
+        new Thread(){
+            @Override
+            public void run() {
+                while(!serv.isClosed()) {
+                    try {
+                        System.out.println("DEBUG: Server in attesa");
+                        Socket client = serv.accept();
+                        System.out.println("DEBUG: Client connesso");
+
+                        OutputStream clientOut = client.getOutputStream();
+                        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientOut));
+
+                        writer.write("Connessione effettuata sul server.");
+                        
+                        writer.close();
+                        client.close();
+                        System.out.println("DEBUG: Connessione terminata");
+                    } catch(IOException ie) {
+                        System.err.println("Qualcosa e' andato storto (Server thread)");
+                        Thread.currentThread().interrupt();
+                    }
+                } 
+            }
+        }.start();
     }
 
     @Override
     public void stopConnection() {
-
+        if(this.serv == null) return;
+        
+        try {
+            this.serv.close();
+        } catch (IOException ie) {
+            System.err.println("Qualcosa e' andato storto (Chiusura del server)");
+        }
     }
-
 }
