87fcc7ea85f8048751d767a1f8f6ed109c71fcbe
AlmondFlame1287 <mpagano284@gmail.com>
2024-07-15 11:41:57 +0200

    Code refactor

--- src/main/java/org/mike/gui/ContentPane.java
+++ src/main/java/org/mike/gui/ContentPane.java
@@ -1,15 +1,18 @@
 package org.mike.gui;
 
+import org.mike.User;
 import org.mike.gui.content.ChatView;
 import org.mike.gui.content.ContactView;
 
 import javax.swing.*;
 
 public class ContentPane extends JSplitPane {
-    private ChatView chatView = new ChatView();
-    private ContactView contactView = new ContactView();
+    private ChatView chatView;
+    private ContactView contactView;
 
-    public ContentPane() {
+    public ContentPane(User user) {
+        chatView = new ChatView();
+        contactView = new ContactView(user);
         this.setLeftComponent(contactView);
         this.setRightComponent(chatView);
 
