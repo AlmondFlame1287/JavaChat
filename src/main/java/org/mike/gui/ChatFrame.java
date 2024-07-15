b4cc1020f0760179a488f6aeb91442920c5e540d
AlmondFlame1287 <mpagano284@gmail.com>
2024-07-15 11:39:47 +0200

    Added JMenuBar with Contacts and Add

--- src/main/java/org/mike/gui/ChatFrame.java
+++ src/main/java/org/mike/gui/ChatFrame.java
@@ -1,5 +1,7 @@
 package org.mike.gui;
 
+import org.mike.User;
+
 import javax.swing.*;
 
 public class ChatFrame extends JFrame {
@@ -9,6 +11,26 @@ public class ChatFrame extends JFrame {
         this.setContentPane(new LoginPane(this));
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        this.setupMenuBar();
         this.setVisible(true);
+    }
+
+    private void setupMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu contactsMenu = new JMenu("Contacts");
+        JMenuItem addContact = new JMenuItem("Add");
+
+        menuBar.add(contactsMenu);
+        contactsMenu.add(addContact);
+
+        this.setJMenuBar(menuBar);
+        addContact.addActionListener(evt -> this.addContactPressed());
+    }
+
+    private void addContactPressed() {
+        if(User.getUser().getName() == null)
+            return;
+
+        new AddContactDialog();
     }
 }
