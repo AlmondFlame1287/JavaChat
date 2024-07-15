4ca8d9349d96bb979189f9f0e0bebc4dbbcf728c
AlmondFlame1287 <mpagano284@gmail.com>
2024-07-15 11:40:00 +0200

    Added Constants class

---
+++ src/main/java/org/mike/common/Constants.java
@@ -0,0 +1,6 @@
+package org.mike.common;
+
+public record Constants() {
+    public static final String USER_DIR = System.getProperty("user.home");
+    public static final String CHAT_PATH = USER_DIR + "/JavaChat";
+}
