22d5ec2aab79ec5c9a4513410c6e96c38fd6d636
AlmondFlame1287 <mpagano284@gmail.com>
2024-07-15 11:39:28 +0200

    Created AddContactDialog

---
+++ src/main/java/org/mike/gui/AddContactDialog.java
@@ -0,0 +1,57 @@
+package org.mike.gui;
+
+import org.mike.Contact;
+import org.mike.User;
+
+import javax.swing.*;
+
+public class AddContactDialog extends JDialog {
+    private JTextField name;
+    private JTextField ip;
+
+    public AddContactDialog() {
+        this.setLayout(null);
+        this.setSize(300, 300);
+        this.initText();
+        this.initButtons();
+        this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    private void initText() {
+        final JLabel nameLabel = new JLabel("Contact name:");
+        final JLabel ipLabel = new JLabel("IP:");
+
+        this.name = new JTextField();
+        this.ip = new JTextField();
+
+        this.add(nameLabel);
+        this.add(name);
+        this.add(ipLabel);
+        this.add(ip);
+
+        nameLabel.setBounds(100, 100, 200, 20);
+        name.setBounds(100, 120, 100, 20);
+
+        ipLabel.setBounds(100, 140, 200, 20);
+        ip.setBounds(100, 160, 100, 20);
+    }
+
+    private void initButtons() {
+        final JButton done = new JButton("Done");
+        final JButton cancel = new JButton("Cancel");
+
+        this.add(done);
+        this.add(cancel);
+
+        done.setBounds(100, 180, 100, 20);
+        cancel.setBounds(100, 200, 100, 20);
+
+        done.addActionListener(evt -> {
+            User.getUser().appendContactToFile(new Contact(name.getText(), ip.getText()));
+            this.dispose();
+        });
+
+        cancel.addActionListener(evt -> this.dispose());
+    }
+}
