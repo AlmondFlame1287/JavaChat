da9b72b4a6d0f88ed09e17011def7eea02a6cc29
AlmondFlame1287 <mpagano284@gmail.com>
2024-07-15 11:42:54 +0200

    WIP: Created a prototype for Message class

--- src/main/java/org/mike/Message.java
+++ src/main/java/org/mike/Message.java
@@ -1,4 +1,33 @@
 package org.mike;
 
-public class Message {
+import org.mike.gui.Drawable;
+
+import java.awt.*;
+import java.util.Date;
+
+public class Message implements Drawable {
+    public static final int MESSAGE_CHARACTER_LIMIT = 300;
+
+    private final String message;
+    private final Date dateTime;
+    private final String sender;
+    private final Rectangle drawingRectangle;
+
+    public Message(Date dateTime, String message, String sender) {
+        this.dateTime = dateTime;
+        this.message = message;
+        this.sender = sender;
+        // TODO: This is only the default message rectangle. Make it resize itself for longer messages
+        this.drawingRectangle = new Rectangle(10, 70, 400, 60);
+    }
+
+    @Override
+    public void draw(Graphics g) {
+
+    }
+
+    @Override
+    public String toString() {
+        return this.dateTime + "," + this.message + "," + this.sender;
+    }
 }
