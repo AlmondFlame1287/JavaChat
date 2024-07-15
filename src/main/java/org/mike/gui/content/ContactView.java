b4c3a639fe3ad1bfc271a2eb9127bb53ba6d5feb
AlmondFlame1287 <mpagano284@gmail.com>
2024-07-15 11:41:02 +0200

    Added Contact reading from file + Drawing on screen

--- src/main/java/org/mike/gui/content/ContactView.java
+++ src/main/java/org/mike/gui/content/ContactView.java
@@ -1,20 +1,59 @@
 package org.mike.gui.content;
 
 import org.mike.Contact;
+import org.mike.User;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class ContactView extends JPanel implements MouseListener {
+    private User currentUser;
     private ArrayList<Contact> contacts;
 
-    public ContactView() {
-        this.setBackground(Color.BLUE);
+    public ContactView(User user) {
+        this.currentUser = user;
+        this.contacts = this.readContacts();
     }
 
+    private ArrayList<Contact> readContacts() {
+        if(!currentUser.getUserFile().exists())
+            return null;
+        ArrayList<Contact> contactArrayList = new ArrayList<>();
+
+        try(FileReader fr = new FileReader(currentUser.getUserFile()); BufferedReader br = new BufferedReader(fr)) {
+            String line;
+            while((line = br.readLine()) != null) {
+                String[] split = line.split(",");
+                contactArrayList.add(new Contact(split[0], split[1]));
+            }
+        } catch(IOException ioe) {
+            System.err.println("There was a problem reading your contacts: " + ioe.getMessage());
+            return null;
+        }
+
+        return contactArrayList;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        this.drawContacts(g);
+    }
+
+    private void drawContacts(Graphics g) {
+        if(contacts == null)
+            return;
+
+        for(Contact cont : contacts) {
+            cont.draw(g);
+        }
+    }
 
     @Override
     public void mouseClicked(MouseEvent e) {
