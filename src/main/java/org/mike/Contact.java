986b9f5bffa1b150b326b44cb2135899bd52b2e6
AlmondFlame1287 <mpagano284@gmail.com>
2024-07-15 11:40:21 +0200

    Created Contact class and made it Drawable on screen

--- src/main/java/org/mike/Contact.java
+++ src/main/java/org/mike/Contact.java
@@ -1,4 +1,41 @@
 package org.mike;
 
-public class Contact {
+//import java.awt.*;
+
+import org.mike.gui.Drawable;
+
+import java.awt.*;
+
+public class Contact implements Drawable {
+    // TODO: Add a bio
+    // TODO: Add profile picture support
+
+    private String name;
+    private String userIP;
+
+    private final Rectangle rectangle;
+//    private Image profilePicture;
+
+    public Contact(String name, String userIP) {
+        this.name = name;
+        this.userIP = userIP;
+        this.rectangle = new Rectangle(10, 70, 400, 60);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUserIP() {
+        return userIP;
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.drawRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+        rectangle.y += 20;
+        g.drawString(this.name, rectangle.x, rectangle.y);
+        rectangle.y += 30;
+        g.drawString(this.getUserIP(), rectangle.x, rectangle.y);
+    }
 }
