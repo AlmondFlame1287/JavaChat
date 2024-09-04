9adfc5eb32651cb0c71b53557d760a8cbfff499c
Michele Pagano <mpagano284@gmail.com>
2024-09-04 15:23:21 +0200

    Added COMMUNICATION_PORT constant

--- src/main/java/org/mike/common/Constants.java
+++ src/main/java/org/mike/common/Constants.java
@@ -4,4 +4,5 @@ public record Constants() {
     public static final String USER_DIR = System.getProperty("user.home");
     public static final String CHAT_PATH = USER_DIR + "/JavaChat/";
     public static final String CONTACT_MESSAGES_PATH =  CHAT_PATH + "/Messages/";
+    public static final int COMMUNICATION_PORT = 59840;
 }
