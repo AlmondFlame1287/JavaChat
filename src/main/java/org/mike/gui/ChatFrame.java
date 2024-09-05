1784a40aa4e565145ea17d8e67a21839fda7682a
Michele Pagano <mpagano284@gmail.com>
2024-09-05 20:19:36 +0200

    Fetch icon from the web

--- src/main/java/org/mike/gui/ChatFrame.java
+++ src/main/java/org/mike/gui/ChatFrame.java
@@ -1,8 +1,15 @@
 package org.mike.gui;
 
 import org.mike.User;
+import static org.mike.common.Constants.VERSION;
+import static org.mike.common.Constants.CHAT_PATH;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 public class ChatFrame extends JFrame {
     public ChatFrame() {
@@ -10,9 +17,25 @@ public class ChatFrame extends JFrame {
         this.setResizable(false);
         this.setContentPane(new LoginPane(this));
         this.setLocationRelativeTo(null);
+        this.setTitle("JavaChat v" + VERSION);
         this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        this.setupIcon();
         this.setupMenuBar();
         this.setVisible(true);
+    }
+
+    private void setupIcon() {
+        File icon = new File(CHAT_PATH + "icon.jpg");
+        try {
+            if(icon.exists()) {
+                this.setIconImage(ImageIO.read(icon));
+                return;
+            }
+
+            BufferedImage image = ImageIO.read(new URL("https://i.ibb.co/3Mphqtv/icon.jpg"));
+            ImageIO.write(image, "jpg", icon);
+            this.setIconImage(ImageIO.read(icon));
+        } catch (IOException ignored) {}
     }
 
     private void setupMenuBar() {
