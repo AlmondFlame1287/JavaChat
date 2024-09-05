702e645696b1cf7648fcee989b374810d4adc34a
Michele Pagano <mpagano284@gmail.com>
2024-09-05 20:19:49 +0200

    Added version and App title

--- src/main/java/org/mike/common/Constants.java
+++ src/main/java/org/mike/common/Constants.java
@@ -1,6 +1,7 @@
 package org.mike.common;
 
 public final class Constants {
+    public static final String VERSION = "0.0.1a";
     public static final String USER_DIR = System.getProperty("user.home");
     public static final String CHAT_PATH = USER_DIR + "/JavaChat/";
     public static final String CONTACT_MESSAGES_PATH =  CHAT_PATH + "/Messages/";
