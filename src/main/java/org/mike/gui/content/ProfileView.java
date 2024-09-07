bad26c7907e09e26a0d749f636ae3a74ecc84a4a
Michele Pagano <mpagano284@gmail.com>
2024-09-07 14:53:31 +0200

    Added settings and add contact buttons

--- src/main/java/org/mike/gui/content/ProfileView.java
+++ src/main/java/org/mike/gui/content/ProfileView.java
@@ -1,6 +1,8 @@
 package org.mike.gui.content;
 
 import org.mike.User;
+import org.mike.gui.AddContactDialog;
+import org.mike.gui.SettingsDialog;
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,6 +15,7 @@ public class ProfileView extends JPanel {
         this.setLayout(null);
         this.setBorder(BorderFactory.createEtchedBorder());
         this.displayUsername();
+        this.displayButtons();
     }
 
     public void displayUsername() {
@@ -29,9 +32,36 @@ public class ProfileView extends JPanel {
         this.getGraphics().drawImage(scaledPfp, 0, 0, null);
     }
 
+    private void displayButtons() {
+        final JButton addContact = new JButton("Add contact");
+        final JButton settings = new JButton("Settings");
+
+        addContact.setBounds(260, 0, 150, 30);
+        settings.setBounds(260, 30, 150, 30);
+
+        this.add(addContact);
+        this.add(settings);
+
+        addContact.addActionListener(evt -> this.addContactPressed());
+        settings.addActionListener(evt -> this.settingsPressed());
+    }
+
     public static ProfileView getInstance() {
         if(instance == null)
             instance = new ProfileView();
         return instance;
+    }
+
+    private void addContactPressed() {
+        if(User.getUser().getName() == null)
+            return;
+
+        SwingUtilities.invokeLater(AddContactDialog::new);
+    }
+
+    private void settingsPressed() {
+        if(User.getUser().getName() == null) return;
+
+        SwingUtilities.invokeLater(SettingsDialog::new);
     }
 }
