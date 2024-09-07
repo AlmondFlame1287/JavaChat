a68db48aff383e34ff3ead4582a714105879ca23
Michele Pagano <mpagano284@gmail.com>
2024-09-07 19:25:36 +0200

    Updated Version
    Signed-off-by: Michele Pagano <mpagano284@gmail.com>

--- src/main/java/org/mike/common/Constants.java
+++ src/main/java/org/mike/common/Constants.java
@@ -1,10 +1,10 @@
 package org.mike.common;
 
 public final class Constants {
-    public static final String VERSION = "0.0.1a";
+    public static final String VERSION = "0.0.2a";
     public static final String USER_DIR = System.getProperty("user.home");
     public static final String CHAT_PATH = USER_DIR + "/JavaChat/";
-    public static final String CONTACT_MESSAGES_PATH =  CHAT_PATH + "/Messages/";
+    public static final String CONTACT_MESSAGES_PATH =  CHAT_PATH + "Messages/";
     public static final int COMMUNICATION_PORT = 59840;
     public static final int PICTURE_PORT = 59841;
 }
