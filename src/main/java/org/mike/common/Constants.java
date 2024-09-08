f22639d13f7e715b71ba0dfca1713c721c02aaf9
Michele Pagano <mpagano284@gmail.com>
2024-09-08 23:23:48 +0200

    Auto sizing variables
    Signed-off-by: Michele Pagano <mpagano284@gmail.com>

--- src/main/java/org/mike/common/Constants.java
+++ src/main/java/org/mike/common/Constants.java
@@ -1,10 +1,19 @@
 package org.mike.common;
 
+import java.awt.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public final class Constants {
-    public static final String VERSION = "0.0.2a";
+    public static final int COMMON_WIDTH = Toolkit.getDefaultToolkit().getScreenSize().width / 2;
+    public static final int COMMON_HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().height / 2;
+
+    public static final int WIDTH_DIVIDE_FACTOR = 4;
+    public static final int HEIGHT_DIVIDE_FACTOR = 8;
+    public static final int RIGHT_COMPONENTS_WIDTH = ((COMMON_WIDTH / WIDTH_DIVIDE_FACTOR) * WIDTH_DIVIDE_FACTOR) - 1;
+    public static final int LEFT_COMPONENTS_WIDTH = COMMON_WIDTH / WIDTH_DIVIDE_FACTOR;
+
+    public static final String VERSION = "TEST-BUILD 0.0.1.2a";
     public static final Path USER_DIR = Paths.get(System.getProperty("user.home")).normalize();
     public static final Path CHAT_PATH = Paths.get(USER_DIR + "/JavaChat").normalize();
     public static final Path CONTACT_MESSAGES_PATH =  Paths.get(CHAT_PATH + "/Messages/").normalize();
